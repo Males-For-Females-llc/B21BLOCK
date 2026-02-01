ad0109426be5a4b88423b00b0c15a0c9dfcb640b
Madeline Nicole Brown <104762490+Madib036@users.noreply.github.com>
2026-01-31 18:48:57 -0600

    Create IUniswapV3Pool.sol

---
+++ audits/tob/contracts/crytic/echidna/@uniswap/IUniswapV3Pool.sol
@@ -0,0 +1,9 @@
+import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
+
+contract MyContract {
+  IUniswapV3Pool pool;
+
+  function doSomethingWithPool() {
+    // pool.swap(...);
+  }
+}
